62d7663a0919bb96b9e820a0af00b82e4553f1bb
star <chlvkfksquf@gmail.com>
2022-07-30 20:26:56 +0900

    Add meta

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,16 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>내일전시</title>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="author" content="ssxst31"></meta>
+        <meta
+          name="description"
+          content="서울 전시회 정보, 내일 전시에서 한눈에 확인해 보세요."
+        ></meta>
+        <meta
+          name="keywords"
+          content="내일전시,전시회,문화예술,서울전시,서울전시회"
+        />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7947339856088209"
