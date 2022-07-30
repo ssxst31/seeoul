f22c2783aa218f9a447e83835654b04fb49a881b
star <chlvkfksquf@gmail.com>
2022-07-30 20:11:44 +0900

    Add script

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,11 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>내일전시</title>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7947339856088209"
+          crossorigin="anonymous"
+        ></script>
       </Head>
       <Component {...pageProps} />
     </>
