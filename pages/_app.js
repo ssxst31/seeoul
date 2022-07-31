78c8137077a397d06746e4ad6a8d31b055b30a18
star <chlvkfksquf@gmail.com>
2022-07-31 20:29:12 +0900

    Add meta

--- pages/_app.js
+++ pages/_app.js
@@ -25,6 +25,19 @@ function MyApp({ Component, pageProps }) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7947339856088209"
           crossorigin="anonymous"
         ></script>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:title" content="내일전시"></meta>
+        <meta
+          property="og:description"
+          content="서울 전시회 정보, 내일 전시에서 한눈에 확인해 보세요."
+        ></meta>
+        <meta name="twitter:card" content="summary"></meta>
+        <meta name="twitter:title" content="내일전시"></meta>
+        <meta
+          name="twitter:description"
+          content="서울 전시회 정보, 내일 전시에서 한눈에 확인해 보세요."
+        ></meta>
+        <meta name="twitter:domain" content="내일전시"></meta>
         <meta
           name="google-site-verification"
           content="uF5xvolIVD9TIzGeXD9vVLNvnwHNg_Ksi--3WgmZpiI"
