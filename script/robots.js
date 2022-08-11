e0f33a8b5559fc086a55f6c36e7d23cbac71015b
star <chlvkfksquf@gmail.com>
2022-08-11 22:04:45 +0900

    Modify robots

--- script/robots.js
+++ script/robots.js
@@ -3,6 +3,7 @@ import fs from "fs";
 const generatedSitemap = `
 User-agent: *
 Allow: /
+Sitemap: https://seeoul.netlify.app/sitemap.xml
 `;
 
 fs.writeFileSync("../public/robots.txt", generatedSitemap, "utf8");
