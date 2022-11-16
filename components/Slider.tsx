c9580a251dc7206e782fe32d6f80ce88da7d6758
star <chlvkfksquf@gmail.com>
2022-11-16 12:45:56 +0900

    Add header slider

---
+++ components/Slider.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Link from "next/link";
+
+import useInterval from "hook/useInterval";
+
+export default function Slider() {
+  const [count, setCount] = useState(0);
+  useInterval(() => {
+    if (count < 3) {
+      setCount((count) => count + 1);
+    } else {
+      setCount(0);
+    }
+  }, 3000);
+
+  const slides = [
+    { title: "서수영 개인전", link: "http://www.helenandjae.com" },
+    { title: "김미경 : Resonance", link: "http://www.galleryluan.com" },
+    {
+      title: "우주 백패킹",
+      link: "http://www.ddp.or.kr/index.html?menuno=230",
+    },
+    {
+      title: "DDP-NFT 기획전",
+      link: "http://www.ddp.or.kr/index.html?menuno=244",
+    },
+  ];
+
+  return (
+    <div style={{ overflowY: "hidden", height: 60 }}>
+      <div
+        style={{
+          transform: `translateY(${-count * 64}px)`,
+          transition: "all ease 2s 0s",
+        }}
+      >
+        {slides.map((el) => (
+          <Link href={el.link} passHref>
+            <a target="_blank" rel="noopener noreferrer">
+              <div style={{ fontSize: 17, fontWeight: 700, color: "#000000" }}>
+                {el.title}
+              </div>
+            </a>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 
 import s from "components/main.module.css";
+import Slider from "components/Slider";
 
 export default function Header() {
   return (
@@ -24,54 +25,58 @@ export default function Header() {
           maxWidth: "1280px",
           margin: "0 auto",
           padding: "0 30px",
+          justifyContent: "space-between",
         }}
       >
-        <a
-          style={{
-            fontSize: 32,
-            color: "#5553FF",
-            marginRight: 50,
-            display: "flex",
-            alignContent: "center",
-          }}
-          href="/"
-        >
-          <img
-            src="/logo.png"
+        <div style={{ display: "flex" }}>
+          <a
             style={{
-              width: 40,
-              height: 40,
-              position: "relative",
-              top: 10,
-              marginRight: 12,
+              fontSize: 32,
+              color: "#5553FF",
+              marginRight: 50,
+              display: "flex",
+              alignContent: "center",
             }}
-            alt="logo"
-          />
-          <div>내일 전시</div>
-        </a>
-        <Link href="/popular">
-          <a>
-            <div>
-              <span
-                style={{ color: "#222222", fontSize: 15 }}
-                className={s.bold}
-              >
-                인기 전시회
-              </span>
-              <img
-                src="/new.png"
-                style={{
-                  width: 16,
-                  height: 16,
-                  position: "relative",
-                  top: 15,
-                  left: -5,
-                }}
-                alt="new"
-              />
-            </div>
+            href="/"
+          >
+            <img
+              src="/logo.png"
+              style={{
+                width: 40,
+                height: 40,
+                position: "relative",
+                top: 10,
+                marginRight: 12,
+              }}
+              alt="logo"
+            />
+            <div>내일 전시</div>
           </a>
-        </Link>
+          <Link href="/popular">
+            <a>
+              <div>
+                <span
+                  style={{ color: "#222222", fontSize: 15 }}
+                  className={s.bold}
+                >
+                  인기 전시회
+                </span>
+                <img
+                  src="/new.png"
+                  style={{
+                    width: 16,
+                    height: 16,
+                    position: "relative",
+                    top: 15,
+                    left: -5,
+                  }}
+                  alt="new"
+                />
+              </div>
+            </a>
+          </Link>
+        </div>
+        <Slider />
       </div>
     </header>
   );
