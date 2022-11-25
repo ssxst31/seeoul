bdd88e4869a6ee0a6128d42bbe4b94a7c9aebd1d
star <chlvkfksquf@gmail.com>
2022-11-25 21:56:48 +0900

    Clean script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -50,21 +50,21 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="Yeti" content="All" />
         <meta name="Yeti" content="index,follow" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="author" content="ssxst31"></meta>
-        <meta name="description" content="서울 전시회 정보, 내일 전시에서 한눈에 확인해 보세요."></meta>
+        <meta name="author" content="ssxst31" />
+        <meta name="description" content="서울 전시회 정보, 내일 전시에서 한눈에 확인해 보세요." />
         <meta name="keywords" content="내일전시,전시회,문화예술,서울전시,서울전시회" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7947339856088209"
           crossOrigin="anonymous"
-        ></script>
-        <meta property="og:type" content="website"></meta>
-        <meta property="og:title" content="내일전시"></meta>
-        <meta property="og:description" content="서울 전시회 정보, 내일 전시에서 한눈에 확인해 보세요."></meta>
-        <meta name="twitter:card" content="summary"></meta>
-        <meta name="twitter:title" content="내일전시"></meta>
-        <meta name="twitter:description" content="서울 전시회 정보, 내일 전시에서 한눈에 확인해 보세요."></meta>
-        <meta name="twitter:domain" content="내일전시"></meta>
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="내일전시" />
+        <meta property="og:description" content="서울 전시회 정보, 내일 전시에서 한눈에 확인해 보세요." />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="내일전시" />
+        <meta name="twitter:description" content="서울 전시회 정보, 내일 전시에서 한눈에 확인해 보세요." />
+        <meta name="twitter:domain" content="내일전시" />
         <meta name="google-site-verification" content="uF5xvolIVD9TIzGeXD9vVLNvnwHNg_Ksi--3WgmZpiI" />
         <meta name="naver-site-verification" content="5b5b59ee6b2ffa4da4adfa1467e7b6321410a63b" />
         <script
