286598c65100498a959b16b62f55e510e604fef6
star <chlvkfksquf@gmail.com>
2022-12-21 12:59:49 +0900

    Fix head title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>내일전시</title>
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7947339856088209"
