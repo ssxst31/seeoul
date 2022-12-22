cae73507b2625a2d59f8d732e45bed1ae05868c2
star <chlvkfksquf@gmail.com>
2022-12-22 18:16:45 +0900

    Fix data

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,25 +6,28 @@ import Main from "components/Main";
 import DefaultLayout from "layouts/DefaultLayout";
 
 export default function HomepageLayout() {
-  function addProductJsonLd() {
-    return {
-      __html: `{
-        "@context": "http://schema.org",
-        "@type": "Organization",
-        "name": "홈 - 내일전시",
-        "url": "https://seeoul.netlify.app",
-        "sameAs": [
-          "https://www.instagram.com/seeou1",
-          "https://twitter.com/seeou11",
-          "https://www.facebook.com/profile.php?id=100083987572734",
-        ]
-      }`,
-    };
-  }
+  const content = {
+    "@context": "http://schema.org",
+    "@type": "Organization",
+    name: "홈 - 내일전시",
+    url: "https://seeoul.netlify.app",
+    sameAs: [
+      "https://www.instagram.com/seeou1",
+      "https://twitter.com/seeou11",
+      "https://www.facebook.com/profile.php?id=100083987572734",
+    ],
+  };
+
   return (
     <>
       <Head>
-        <script type="application/ld+json" dangerouslySetInnerHTML={addProductJsonLd()} key="product-jsonld" />
+        <Script
+          id="app-ld-json"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(content, null, "\t"),
+          }}
+        />
       </Head>
       <DefaultLayout>
         <Main />
