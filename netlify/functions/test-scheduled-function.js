daff1a7ce0531ede7183cfa721b6496771967897
star <chlvkfksquf@gmail.com>
2023-01-09 13:55:52 +0900

    schedule test

--- netlify/functions/test-scheduled-function.js
+++ netlify/functions/test-scheduled-function.js
@@ -3,7 +3,7 @@ import { schedule } from "@netlify/functions";
 
 const BUILD_HOOK = "https://api.netlify.com/build_hooks/63bb90ab0e674d226f545a62";
 
-const handler = schedule("54 13 * * *", async () => {
+const handler = schedule("57 13 * * *", async () => {
   await fetch(BUILD_HOOK, {
     method: "POST",
   }).then((response) => {
