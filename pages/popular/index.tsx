ff05aeb9f2bc12302e267df2765a560df0c18f8c
star <chlvkfksquf@gmail.com>
2023-01-13 01:14:39 +0900

    Modify popular card style

--- pages/popular/index.tsx
+++ pages/popular/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 import { GetServerSideProps } from "next";
 import { InstagramFeed } from "type";
@@ -34,15 +35,19 @@ export default function Popular({ instagramFeed }: PopularProps) {
         <Header />
         <div className="pt-40 px-[30px] -md:pt-[200px] -md:px-4">
           <span className="text-2xl font-bold">요즘 핫한 전시회 ! 🫧</span>
-          <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          <div className="grid grid-cols-1 gap-y-8 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 -md:gap-y-4 -md:gap-x-4">
             {instagramFeed.map((i, index) => (
-              <div
-                className="w-full overflow-hidden border border-gray-200 shadow-lg aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 shadow-gray-100"
-                key={index}
-              >
+              <div className="shadow-lg aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 shadow-gray-100" key={index}>
                 <Link href={i.permalink} passHref>
                   <a target="_blank" rel="noopener noreferrer">
-                    <img src={i.media_url} className="w-full h-full" />
+                    <div className="relative w-full overflow-hidden h-96">
+                      <Image
+                        src={i.media_url}
+                        alt={i.caption}
+                        layout="fill"
+                        className="duration-100 ease-linear hover:scale-110"
+                      />
+                    </div>
                   </a>
                 </Link>
               </div>
