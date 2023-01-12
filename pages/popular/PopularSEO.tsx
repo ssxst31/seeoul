a3f9990327586ca9177038519c094bd5ba233e16
star <chlvkfksquf@gmail.com>
2023-01-12 18:12:34 +0900

    Modify popularSEO

--- pages/popular/PopularSEO.tsx
+++ pages/popular/PopularSEO.tsx
@@ -4,6 +4,7 @@ import { NextSeo } from "next-seo";
 export default function PopularSEO() {
   const NEXT_SEO = {
     title: "인기전시회 | 내일전시",
+    description: "내일전시. 요즘 인기있는 전시회 알아보기",
   };
 
   return <NextSeo {...NEXT_SEO} />;
