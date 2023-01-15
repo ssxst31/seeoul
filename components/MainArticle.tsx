9f76aa86525d2c0bc20507d7d6e5a993fc87d36b
star <chlvkfksquf@gmail.com>
2023-01-15 20:53:32 +0900

    Add Skeleton

--- components/MainArticle.tsx
+++ components/MainArticle.tsx
@@ -2,12 +2,17 @@ import React from "react";
 
 import { CulturalEvent } from "type";
 import CulturalEventCard from "components/CulturalEventCard";
+import Skeleton from "components/Skeleton";
 
 interface MainArticleProps {
   totalCulturalEvent: CulturalEvent[] | [] | null;
 }
 
 export default function MainArticle({ totalCulturalEvent }: MainArticleProps) {
+  if (!totalCulturalEvent) {
+    return <Skeleton height="418" row={16} />;
+  }
+
   if (totalCulturalEvent?.length === 0) {
     return <div className="inline-flex items-center justify-center w-full h-20">검색 결과가 없습니다.</div>;
   }
@@ -15,7 +20,7 @@ export default function MainArticle({ totalCulturalEvent }: MainArticleProps) {
   return (
     <article>
       <div className="grid grid-cols-1 gap-y-8 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 -md:gap-y-4 -md:gap-x-4">
-        {totalCulturalEvent?.map((culturalEvent, index) => (
+        {totalCulturalEvent.map((culturalEvent, index) => (
           <CulturalEventCard culturalEvent={culturalEvent} key={culturalEvent.id} index={index} />
         ))}
       </div>
