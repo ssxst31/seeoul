303e0508ef9ce85d104d069c8784a7cb042c1929
star <chlvkfksquf@gmail.com>
2023-01-16 17:34:27 +0900

    Modify pagination

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -24,7 +24,7 @@ const makePaginationHref = (totalCount: number, page: number, tab: string) => {
     pageNumbersArray.push(
       <Link href={`?tab=${tab}&page=${page - 1}`} passHref shallow={true} scroll={true}>
         <a>
-          <div className="inline-flex items-center justify-center w-10 h-10 text-gray-500 border border-gray-200 rounded-md -md:w-8 -md:h-8">
+          <div className="inline-flex items-center justify-center w-10 h-10 text-gray-500 border border-gray-200 rounded-md -md:w-8 -md:h-8 hover:border-indigo-600 ">
             &lt;
           </div>
         </a>
@@ -37,11 +37,8 @@ const makePaginationHref = (totalCount: number, page: number, tab: string) => {
       <Link href={`?tab=${tab}&page=${i}`} passHref shallow={true} scroll={true}>
         <a>
           <div
-            className={
-              page === i
-                ? "border-indigo-600 text-indigo-600 w-10 h-10  border inline-flex justify-center items-center rounded-md -md:w-8 -md:h-8"
-                : "w-10 h-10 border-gray-200 border inline-flex justify-center items-center rounded-md text-gray-500 -md:w-8 -md:h-8"
-            }
+            className={`-md:w-8 -md:h-8 rounded-md inline-flex justify-center items-center border w-10 h-10 hover:border-indigo-600 
+            ${page === i ? "border-indigo-600 text-indigo-600" : "border-gray-200 text-gray-500 "}`}
           >
             {i}
           </div>
@@ -54,7 +51,7 @@ const makePaginationHref = (totalCount: number, page: number, tab: string) => {
     pageNumbersArray.push(
       <Link href={`?tab=${tab}&page=${page + 1}`} passHref shallow={true} scroll={true}>
         <a>
-          <div className="inline-flex items-center justify-center w-10 h-10 text-gray-500 border border-gray-200 rounded-md -md:w-8 -md:h-8">
+          <div className="inline-flex items-center justify-center w-10 h-10 text-gray-500 border border-gray-200 rounded-md -md:w-8 -md:h-8 hover:border-indigo-600">
             &gt;
           </div>
         </a>
