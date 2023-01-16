9f17911ad82a7dbb1a9c5af73a40c02b12fce173
star <chlvkfksquf@gmail.com>
2023-01-16 16:02:38 +0900

    Add reviewSEO

---
+++ pages/review/ReviewSEO.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { NextSeo } from "next-seo";
+
+export default function ReviewSEO() {
+  const NEXT_SEO = {
+    title: "후기 | 내일전시",
+    description: "내일전시. 요즘 인스타에서 핫한 후기",
+  };
+
+  return <NextSeo {...NEXT_SEO} />;
+}
