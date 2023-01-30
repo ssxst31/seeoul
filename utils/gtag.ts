3aa9657001e6c45a6c990b77b8b075a49510a20f
star <chlvkfksquf@gmail.com>
2023-01-30 14:07:01 +0900

    Fix gtag

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -2,7 +2,7 @@ import { isProduction, isServer } from "./env";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
-  if (!isServer && window.gtag && isProduction()) {
+  if (isProduction()) {
     window.gtag("config", process.env.NEXT_PUBLIC_GA_TRACKING_ID as string, {
       page_path: url,
     });
