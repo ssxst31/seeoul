28616d99dea1c1a517f31ed5d50231a2faaf0258
star <chlvkfksquf@gmail.com>
2023-07-10 21:21:33 +0900

    Revert "test"
    This reverts commit 0da4924b98f331efb307c3ee3eccf8dedbefc4e5.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,39 @@
-export default async function Page() {
-  return <>ㅇㅍㅇㄴㅁ</>;
+import MainCarousel from "app/_component/MainCarousel";
+import Section from "app/_component/Section";
+import MainSection from "app/_component/MainSection";
+import BlackButton from "app/_component/BlackButton";
+import { filterSort } from "utils/filterSort";
+
+export default async function Page({ searchParams }: any) {
+  const page = searchParams.page ?? "1";
+  const tab = (searchParams.tab ?? "total") as string;
+  const sort = filterSort((tab as string) ?? "total");
+  const search = searchParams.search ?? undefined;
+
+  return (
+    <main className="pt-[71px] px-[30px] w-full -md:px-4 -md:pt-[104px]">
+      <section>
+        <h2 className="pb-1 text-2xl font-bold text-black dark:text-white linear2">전시회를 생각 중이라면 👀</h2>
+        <div className="w-full h-8 -md:h-4" />
+        <MainCarousel />
+      </section>
+      <div className="w-full h-10" />
+      <section>
+        <h2 className="pb-1 text-2xl font-bold text-black dark:text-white linear2 h-[29px] self-start">
+          어디서 구경할까요? 🤔
+        </h2>
+        <div className="w-full h-7 -md:h-4" />
+        <Section />
+        <div className="w-full h-8 -md:h-4" />
+        {/* @ts-expect-error Async Server Component */}
+        <MainSection page={page} tab={tab} sort={sort} search={search} />
+        <div className="w-full h-8" />
+        <div className="text-center"></div>
+        <div className="w-full h-8" />
+        <div className="max-w-[728px] mx-auto overflow-x-hidden -md:max-w-[300px]"></div>
+        <div className="w-full h-8" />
+      </section>
+      <BlackButton />
+    </main>
+  );
 }
