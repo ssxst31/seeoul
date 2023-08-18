2903aa53bc5ffc665bd40174e9dd11ee1d58eb74
star <chlvkfksquf@gmail.com>
2023-08-18 20:10:02 +0900

    Modify blog api

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -3,34 +3,34 @@ import { notFound } from "next/navigation";
 
 import { getHostName } from "utils/getHostName";
 
-// export async function generateStaticParams() {
-//   return [
-//     { id: "1" },
-//     { id: "2" },
-//     { id: "3" },
-//     { id: "4" },
-//     { id: "5" },
-//     { id: "6" },
-//     { id: "7" },
-//     { id: "8" },
-//     { id: "9" },
-//     { id: "10" },
-//   ];
-// }
+export async function generateStaticParams() {
+  return [
+    { id: "1" },
+    { id: "2" },
+    { id: "3" },
+    { id: "4" },
+    { id: "5" },
+    { id: "6" },
+    { id: "7" },
+    { id: "8" },
+    { id: "9" },
+    { id: "10" },
+  ];
+}
 
 export default async function Page({ params }: { params: { id: string } }) {
-  // const { id } = params;
-  // if (Number(id) > 10) {
-  //   notFound();
-  // }
-  // console.log(`http://localhost:3000/api/blogs/${id}`);
-  // const res = await fetch(`https://seeoul.netlify.app/api/blogs/${id}`, { cache: "force-cache" });
+  const { id } = params;
+  if (Number(id) > 10) {
+    notFound();
+  }
 
-  // const post = (await res.json()) as any;
+  const res = await fetch(`${getHostName}/api/blogs/${id}`, { cache: "force-cache" });
+
+  const post = (await res.json()) as any;
 
   return (
     <div className="px-[30px] w-full -md:pt-36 -md:px-4 pt-20 text-center">
-      {/* <h2 className="text-4xl ">{post.title}</h2>
+      <h2 className="text-4xl ">{post.title}</h2>
       <div className="w-full h-10" />
       <div className="text-left">
         {post.content.map((el: any) => (
@@ -41,7 +41,7 @@ export default async function Page({ params }: { params: { id: string } }) {
       <Link href="/blog" as={`/blog`}>
         <button className="w-48 h-12 text-xl text-white bg-indigo-600 rounded-xl">목록으로 이동</button>
       </Link>
-      <div className="w-full h-10" /> */}
+      <div className="w-full h-10" />
     </div>
   );
 }
