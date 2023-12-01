1f185d3cf190e9f467c4561905ca8dd17d3e8f43
star <chlvkfksquf@gmail.com>
2023-12-01 12:57:39 +0900

    Fix header style

--- layouts/CSRHeader.tsx
+++ layouts/CSRHeader.tsx
@@ -67,7 +67,7 @@ export default function Header() {
       </div>
       {pathname === "/" && (
         <div className="overflow-x-scroll border-t border-gray-100 border-solid scrollbar-hide dark:border-dark-200">
-          <ul className="max-w-7xl px-7 -md:max-w-none -md:w-[765px] -md:px-4 flex mx-auto h-10 items-center space-x-5">
+          <ul className="max-w-7xl px-7 -md:max-w-none -md:w-[807px] -md:px-4 flex mx-auto h-10 items-center space-x-5">
             {TABS.map((t, index) => (
               <li key={index}>
                 <Link href={`/?tab=${t.sort}`} key={index} shallow={true} scroll={true} passHref>
