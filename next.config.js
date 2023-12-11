eb08dfce80dab005fafcc88a54e7be81c4f20ae7
star <chlvkfksquf@gmail.com>
2023-12-11 19:11:10 +0900

    Modify next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
   i18n: {
     locales: ["ko"],
     defaultLocale: "ko",
@@ -12,16 +9,19 @@ const nextConfig = {
   images: {
     deviceSizes: [640, 1024, 1280],
     imageSizes: [16, 64, 96],
-    domains: [
-      "culture.seoul.go.kr",
-      "localhost",
-      "seoul.go.kr",
-      "*",
-      "scontent-gmp1-1.cdninstagram.com",
-      "scontent-ssn1-1.cdninstagram.com",
-      "video-ssn1-1.cdninstagram.com",
-      "seeoul.vercel.app",
-      "shopping-phinf.pstatic.net",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "culture.seoul.go.kr",
+      },
+      {
+        protocol: "https",
+        hostname: "seoul.go.kr",
+      },
+      {
+        protocol: "https",
+        hostname: "shopping-phinf.pstatic.net",
+      },
     ],
   },
 };
